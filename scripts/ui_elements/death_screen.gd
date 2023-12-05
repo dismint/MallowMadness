c2fe62d933d163ccab9f1f538a005fafef3e4343
sr1k1 <riki.smah@gmail.com>
2023-12-05 15:41:50 -0500

    reorganized scripts and ui elements into appropriate folders

--- scripts/ui_elements/death_screen.gd
+++ scripts/ui_elements/death_screen.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_parent().get_node('FinishLine').finish_game.connect(_on_finish_line_finish_game)
@@ -10,7 +11,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_finish_line_finish_game():
 	show()
